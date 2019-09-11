8b6adebdd979f92378adb80b2e76844a90574bd1
m <mleipper@hotmail.com>
2019-09-11 11:52:57 +0100

    Added a helper method to get the numbers

--- squareCalc/Program.cs
+++ squareCalc/Program.cs
@@ -6,7 +6,29 @@ namespace squareCalc
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var num1=helper.getNumber("first");
+            var num2=helper.getNumber("Second");
+            var num3 = helper.getNumber("Third");
+            
+        }
+    }
+
+    public class helper {
+        public static double getNumber(string time)
+        {
+            Console.WriteLine($"Please enter {time} number");
+            var num =Console.ReadLine();
+            if (double.TryParse(num, out double num1))
+            {
+                Console.WriteLine($"The value of your number is {num1}");
+                return num1;
+            }
+            else
+            {
+                Console.WriteLine($"{num} is not a number");
+                var num2 = helper.getNumber(time);
+                return num2;
+            }
         }
     }
 }
