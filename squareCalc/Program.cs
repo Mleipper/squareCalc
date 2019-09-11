442861b9265f440199f5785427da0f2e3964a9a5
m <mleipper@hotmail.com>
2019-09-11 10:55:14 +0100

    Add project files.

---
+++ squareCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace squareCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
